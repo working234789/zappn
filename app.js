848b7cc212777f7abd6cd9186943db1d5b077d56
Igor <working234789@gmail.com>
2021-08-30 00:08:04 +0200

    InitialCommit

---
+++ app.js
@@ -0,0 +1,27 @@
+var express = require("express"),
+    app = express(),
+    port = process.env.PORT || 3000;
+
+app.listen(port);
+
+console.log("zappn running on port " + port);
+	
+
+
+app.get("/ping", (req, res, next) => {
+
+    var today = new Date();
+    var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+    var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds() + "." + today.getMilliseconds();
+    var dateTime = date + ' ' + time;
+    var timestamp = today.valueOf();
+
+    res.json(
+            {
+                "app": "zappn",
+                "ping": "OK",
+                "time": dateTime,
+                "timestamp": timestamp
+            }
+        );
+   });
